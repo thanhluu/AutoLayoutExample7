febe80db61a4140610e5c88dea71710bebb9b629
thanhluu <thanhlt90@gmail.com>
2016-06-27 22:47:48 +0700

    view dictionary

--- AutoLayoutExample7/ViewController.swift
+++ AutoLayoutExample7/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let orangeView = UIView()
+    let blueView = UIView()
+    let purpleView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        orangeView.backgroundColor = UIColor(red: 255/255.0, green: 148/255.0, blue: 0.0, alpha: 1.0)
+        blueView.backgroundColor = UIColor(red: 187/255.0, green: 44/255.0, blue: 162/255.0, alpha: 1.0)
+        purpleView.backgroundColor = UIColor(red: 122/255.0, green: 206/255.0, blue: 255/255.0, alpha: 1.0)
+        
+        orangeView.translatesAutoresizingMaskIntoConstraints = false
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        purpleView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(orangeView)
+        view.addSubview(blueView)
+        view.addSubview(purpleView)
+        
+        let views: [String: AnyObject] = [
+            "orangeView": orangeView
+        ]
+        
+        let metrics: [String: AnyObject] = [
+            "orangeViewWidth": 200
+        ]
+        
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:[orangeView(orangeViewWidth)]", options: [], metrics: metrics, views: views))
     }
 
     override func didReceiveMemoryWarning() {
